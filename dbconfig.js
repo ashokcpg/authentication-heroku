0563edce304241455fa131df6b738f7bae4f005d
styrene <styrene@styrene.styrene>
2021-07-27 12:55:56 +0545

    Heroku Push Take 2

--- dbconfig.js
+++ dbconfig.js
@@ -1,17 +1,18 @@
-const knex = require('knex')
-require('dotenv').config()
+const knex = require("knex");
+require("dotenv").config();
 
 const db = knex({
-    client:'pg',
-    connection: {
+	client: "pg",
+	connection: {
 		host: process.env.DB_HOST,
 		ssl: {
 			rejectUnauthorized: false,
 		},
 		user: process.env.DB_USER,
-		password:process.env.DB_PASSWORD,
+		password: process.env.DB_PASSWORD,
 		database: process.env.DB_DATABASE,
-	}});
+	},
+});
 module.exports = db;
 
 // const db = knex({
