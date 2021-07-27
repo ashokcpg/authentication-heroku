f5baf12b871bcbdc059e9ba09b8c2fe17a8045dd
styrene <styrene@styrene.styrene>
2021-07-27 23:14:56 +0545

    Added Routes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const db = require("./dbconfig.js");
 const express = require("express");
 const app = express();
+// Express middlewares
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Importing Routes
+
 const Register = require("./routes/Register.js");
 const Login = require("./routes/Login.js");
 
+// Connecting to the Database,
 app.listen(process.env.PORT || 3000, async () => {
 	db.select("*")
 		.from("cred")
@@ -17,14 +21,14 @@ app.listen(process.env.PORT || 3000, async () => {
 		.catch((err) => {
 			console.log("Failed to connect to the database," + err);
 		});
-	console.log("Server running at port 3000");
+	console.log(`Server running at port ${process.env.PORT || 3000}`);
 });
 
-// Setting endpoints
-app.use(express.json());
+// Setting Root View,
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/views/home.html");
 });
 
+// Setting up the route handlers,
 app.use("/register", Register);
 app.use("/login", Login);
